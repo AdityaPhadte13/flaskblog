3c79a4cfbfed1fb0c61ff18edff433119d2e751c
Aditya Phadte <adityaphadte13@gmail.com>
2019-09-05 00:54:37 +0530

    Image upload and update form added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask, escape
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '588bfc6c02c11378a5e620b0bd1dfb43'
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,7 +1,12 @@
-from flaskblog import db
+from flaskblog import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(20), unique=True, nullable=False)
@@ -10,7 +15,7 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"    
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
